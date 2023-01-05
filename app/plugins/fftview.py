1bc36dd714bedc9572671af34d8258919b34305e
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2023-01-05 09:17:25 -0500

    Clamp max X to lowest freq

--- app/plugins/fftview.py
+++ app/plugins/fftview.py
@@ -14,4 +14,7 @@ class FFTView(DataView):
         fft: pd.DataFrame = ed.calc.fft.fft(df)
         # Remove all negative frequencies since it's a mirror
         fft = fft[fft.index >= 1]
+        # If the max frequency is less than the default max x value
+        # update the max x value to the max frequency
+        self.x_range = (0, min(self.x_range[1], fft.index.max()))
         return fft
--- app/plugins/srsview.py
+++ app/plugins/srsview.py
@@ -10,5 +10,10 @@ class SRSView(DataView):
     y_title = "Magnitude"
 
     def generate(self, df: pd.DataFrame) -> pd.DataFrame:
-        srs = endaq.calc.shock.shock_spectrum(df, damp=0.05, init_freq=1, mode="srs")
+        srs: pd.DataFrame = endaq.calc.shock.shock_spectrum(
+            df, damp=0.05, init_freq=1, mode="srs"
+        )
+        # If the max frequency is less than the default max x value
+        # update the max x value to the max frequency
+        self.x_range = (0, min(self.x_range[1], srs.index.max()))
         return srs
